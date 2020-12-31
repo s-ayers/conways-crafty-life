5959c24bdd11a466ab810e6f2a3e0643bbcb369f
bluegeek9 <steveneayers@gmail.com>
2020-12-31 16:13:59 -0600

    Angular migration

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import './style.css';
-
-import * as boilerplate from './boilerplate';
-// import * as jquery from '../lib/jquery.min.js';
-import * as crafty from '../lib/crafty.js';
-import * as game from './game';
-import * as components from './components';
